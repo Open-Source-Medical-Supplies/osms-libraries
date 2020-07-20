edcfc9b51d612221d9e178f9e098665490b9d6d7
josh <hello@joshmleslie.com>
2020-07-20 18:45:37 -0400

    make ctor / fn comparison explicit vs programatic - bc a possible bug during dev deployment

--- src/services/language.service.ts
+++ src/services/language.service.ts
@@ -3,15 +3,20 @@ import { SET_LANG } from "../redux/lang.reducer";
 import { BasicObject } from "../types/shared.type";
 import { AirtableHelpers, AirtableSupportingCalls } from "./airtable";
 
-const loadStaticLanguage = async (dispatch: Dispatch<any>): Promise<void>  => {
+const langReducer = (data: any) => {
   const locale = navigator.language;
+  return data.reduce((acc: BasicObject<string>, val: BasicObject<string>) => {
+    const {key, ...langs} = val;
+    acc[key] = langs[locale] || langs['en-US'];
+    return acc;
+  }, {});
+}
+
+const loadStaticLanguage = async (dispatch: Dispatch<any>): Promise<void>  => {
 	const langData = await AirtableHelpers.callATbase(
 		AirtableSupportingCalls.getStaticLanguage,
-		(data: any) => data.reduce((acc: BasicObject<string>, val: BasicObject<string>) => {
-      const {key, ...langs} = val;
-      acc[key] = langs[locale] || langs['en-US'];
-      return acc;
-    }, {})
+    langReducer,
+    false
 	);
 	dispatch({type: SET_LANG, payload: langData})
 };
