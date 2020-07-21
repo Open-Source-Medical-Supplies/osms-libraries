4c6324d01541536bdeeac276f6640c0512cae8fc
josh <hello@joshmleslie.com>
2020-07-21 09:45:40 -0400

    Add fix for WP nuking list bullets Add fix for react-md nesting larger blocks of list-el text in paragraphs, throwing off alignment with bullet

---
+++ src/shared/components/markdown/list-item-handler.tsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent, ReactElement } from 'react';
+import ReactMarkdown, { Renderer } from 'react-markdown';
+
+function isParagraphChild(el: ListItemHandler): boolean {
+  return (
+    el.props && el.props.children &&
+    el.props.children instanceof Array &&
+    el.props.children[0] &&
+    (
+      el.props.children[0].type === 'p' ||
+      el.props.children[0].key.includes('paragraph')
+    )
+  );
+}
+
+function paragraphToListItem (el: ListItemHandler): ReactElement {
+  return el.props.children[0].props;
+}
+
+class ListItemHandler extends PureComponent {
+  static defaultProps = {
+    language: ''
+  };
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    language: PropTypes.string
+  };
+
+  props: any;
+
+  constructor(props: any) {
+    super(props);
+    console.log(this)
+  }
+  
+  render() {
+    const renderProps = isParagraphChild(this) ?
+                    paragraphToListItem(this) : this.props;
+    return (ReactMarkdown.renderers.listItem as Renderer<any>)(renderProps);
+  }
+}
+
+export default ListItemHandler;
+
