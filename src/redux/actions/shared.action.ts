9f829593c4a3e2cb995a9de6f80e7f25992e5064
Josh <hello@joshmleslie.com>
2020-09-26 17:49:15 -0400

    fix wrong lib-change so filters weren't clearing refactoring

--- src/redux/actions/shared.action.ts
+++ src/redux/actions/shared.action.ts
@@ -5,7 +5,7 @@ import ActiveLib from "../../shared/types/lib.enum";
 import { Selected } from "../../shared/types/selected.type";
 import { TypedThunkAction } from "../root.reducer";
 import { setCategories } from "./filter.action";
-import { changeLib, setLib } from "./lib.action";
+import { changeLib } from "./lib.action";
 import { setSelected, setSelectedByName } from "./selected.action";
 
 export const filterFromCategoryToProjects = (
@@ -19,31 +19,24 @@ export const filterFromCategoryToProjects = (
   );
 };
 
-export const libToProjAndSetFilter = (
-  displayName: string
-): TypedThunkAction => (dispatch, _) => {
-  dispatch(setLib(ActiveLib.PROJECT));
-  dispatch(
-    setCategories({
-      [displayName]: true,
-    })
-  );
-};
-
 export const libToCatAndSelectCategory = (
   displayName: string,
   selected: Selected
 ): TypedThunkAction => (dispatch, _) => {
-  dispatch(setLib(ActiveLib.CATEGORY));
-  dispatch(
-    setSelectedByName(
-      displayName,
-      "displayName",
-      TABLE_MAPPING.CategoryInfo,
-      ActiveLib.CATEGORY,
-      selected
-    )
-  );
+  new Promise((r) => {
+    dispatch(changeLib(ActiveLib.CATEGORY));
+    r();
+  }).then(() => {
+    dispatch(
+      setSelectedByName(
+        displayName,
+        "displayName",
+        TABLE_MAPPING.CategoryInfo,
+        ActiveLib.CATEGORY,
+        selected
+      )
+    );
+  });
 };
 
 export const linkAcross = (
@@ -51,26 +44,19 @@ export const linkAcross = (
   displayName: string,
   origin?: Selected
 ): TypedThunkAction => (dispatch, _) => {
-  new Promise((r) => {
-    if (selected instanceof Project) {
-      dispatch(libToProjAndSetFilter(displayName));
-    } else if (selected instanceof CategoryInfo) {
-      dispatch(setLib(ActiveLib.CATEGORY));
-    }
-    r();
-  }).then(() => {
-    // for race issue
-    if (selected instanceof Project) {
-      dispatch(setSelected(selected, ActiveLib.PROJECT, origin));
-    } else if (selected instanceof CategoryInfo) {
-      dispatch(
-        setSelectedByName(
-          displayName,
-          "displayName",
-          TABLE_MAPPING.CategoryInfo,
-          ActiveLib.CATEGORY
-        )
-      );
-    }
-  });
+  const toLib = selected instanceof Project ? ActiveLib.PROJECT : ActiveLib.CATEGORY;
+
+  dispatch(changeLib(toLib));
+
+  if (selected instanceof Project) {
+    // sets selected category as filter after jump to project lib
+    dispatch(setCategories({ [displayName]: true }));
+  }
+
+  // for race issue
+  setTimeout(() => dispatch(setSelected(
+    selected,
+    toLib,
+    origin
+  )));
 };
